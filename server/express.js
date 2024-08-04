686ccdb0378425ba3ee5fe5d1a237884594aa046
techgalsG8 <techgalsG8@gmail.com>
2024-08-04 02:29:31 -0400

    message

--- server/express.js
+++ server/express.js
@@ -17,7 +17,7 @@ const app = express();
 
 const CURRENT_WORKING_DIR = process.cwd();
 
-app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')));
+app.use(express.static(path.join(CURRENT_WORKING_DIR, "dist/app")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
